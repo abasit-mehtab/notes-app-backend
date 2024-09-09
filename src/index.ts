3f53d70284aaa778da020220c3799765edceda99
abasit-mehtab <abasit.mehtab@getweys.com>
2024-09-09 16:21:36 +0500

    changed port

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
 import { Request, Response } from "express";
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,6 +19,6 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use("/api", authRoutes);
 
-app.listen(3001, () => {
-  console.log("Server is running on port 3001");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
